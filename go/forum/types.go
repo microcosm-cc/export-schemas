1c03713204c812a8b591584dc56991409600e403
David Kitchen <david@buro9.com>
2014-05-22 12:29:19 +0100

    Fully commented up, and golint happy

--- go/forum/types.go
+++ go/forum/types.go
@@ -1,39 +1,47 @@
+// Package forum provides the types that encapsulate the idea of a forum or
+// discussion group and the data therein.
 package forum
 
 import "time"
 
-type Id struct {
-	Id int64 `json:"id"`
+// ID represents an identifier of a thing within a forum
+type ID struct {
+	ID int64 `json:"id"`
 }
 
-type Association struct {
-	OnType string `json:"onType,omitempty"`
-	OnId   int64  `json:"onId,omitempty"`
-}
-
+// User represents basic knowledge of a user within a forum. It is presumed that
+// all forums have a concept of a username and that users have an email address
+// and an identifier
 type User struct {
-	Id                        int64     `json:"id"`
+	ID                        int64     `json:"id"`
 	Name                      string    `json:"name"`
 	Email                     string    `json:"email"`
 	DateCreated               time.Time `json:"dateCreated,omitempty"`
 	LastActive                time.Time `json:"lastActive,omitempty"`
-	IpAddress                 string    `json:"ipAddress,omitempty"`
+	IPAddress                 string    `json:"ipAddress,omitempty"`
 	ReceiveEmailFromAdmins    bool      `json:"receiveEmailFromAdmins,omitempty"`
 	ReceiveEmailNotifications bool      `json:"receiveEmailNotifications,omitempty"`
 	Banned                    bool      `json:"isBanned,omitempty"`
-	Usergroups                []Id      `json:"usergroups,omitempty"`
+	Usergroups                []ID      `json:"usergroups,omitempty"`
 }
 
+// Usergroup represents the concept of a group of users that share a set of
+// permissions. Users are included into a usergroup either explicitly (this
+// user is in this group) or implicitly (anyone who has more than 1,500 comments
+// is in this group).
 type Usergroup struct {
-	Id               int64            `json:"id"`
+	ID               int64            `json:"id"`
 	Name             string           `json:"name,omitempty"`
 	Text             string           `json:"text,omitempty"`
 	Banned           bool             `json:"isBanned,omitempty"`
 	Moderator        bool             `json:"isModerator,omitempty"`
 	ForumPermissions ForumPermissions `json:"forumPermissions,omitempty"`
-	Users            []Id             `json:"users,omitempty"`
+	Users            []ID             `json:"users,omitempty"`
+	Criteria         []Criterion      `json:"criteria,omitempty"`
 }
 
+// ForumPermissions describes the permissions that users within a usergroup have
+// on a given forum.
 type ForumPermissions struct {
 	View         bool `json:"canView,omitempty"`
 	PostNew      bool `json:"canPostNew,omitempty"`
@@ -45,8 +53,50 @@ type ForumPermissions struct {
 	OpenOwn      bool `json:"canOpenOwn,omitempty"`
 }
 
+/*
+Criterion describes an implicity inclusion of a User into a Usergroup.
+
+Criterion belonging to the same usergroup are applied according to the
+OrGroup value, where like values are AND and other values are OR.
+
+An example:
+   Criterion{OrGroup: 0, Key: "comments", Predicate "ge", Value: 1500}
+   Criterion{OrGroup: 0, Key: "is_member", Predicate "eq", Value: true}
+   Criterion{OrGroup: 1, Key: "foo", Predicate "eq", Value: "bar"}
+
+Would be applied as:
+   (comments >= 1500 AND is_member == true) OR foo == "bar"
+
+It is the responsibility of an importing system to determine the meaning of
+the Key field.
+*/
+type Criterion struct {
+	OrGroup   int64       `json:"orGroup"`
+	Key       string      `json:"key,omitempty"`
+	Predicate string      `json:"predicate"`
+	Value     interface{} `json:"value,omitempty"`
+}
+
+// PredicateEquals and the other predicates are the range of valid predicates
+// for use in Criterion. Valid predicates are determined by the type of the
+// Criterion Value, equality applies to all but substr predicates only apply to
+// strings.
+const (
+	PredicateEquals              string = "eq"
+	PredicateNotEquals           string = "ne"
+	PredicateLessThan            string = "lt"      // Only on numbers and dates
+	PredicateLessThanOrEquals    string = "le"      // Only on numbers and dates
+	PredicateGreaterThanOrEquals string = "ge"      // Only on numbers and dates
+	PredicateGreaterThan         string = "gt"      // Only on numbers and dates
+	PredicateSubstring           string = "substr"  // Only on strings
+	PredicateNotSubstring        string = "nsubstr" // Only on strings
+)
+
+// Forum represents a group/forum/section of a discussion site. This is the
+// container for content. It is assumened that usergroup permissions are applied
+// generally to the forum level and not to specific items within the forum.
 type Forum struct {
-	Id           int64       `json:"id"`
+	ID           int64       `json:"id"`
 	Name         string      `json:"name"`
 	Text         string      `json:"text,omitempty"`
 	DisplayOrder int64       `json:"displayOrder,omitempty"`
@@ -55,13 +105,14 @@ type Forum struct {
 	Moderated    bool        `json:"isModerated,omitempty"`
 	Deleted      bool        `json:"isDeleted,omitempty"`
 	Usergroups   []Usergroup `json:"usergroups,omitempty"`
-	Moderators   []Id        `json:"moderators,omitempty"`
+	Moderators   []ID        `json:"moderators,omitempty"`
 }
 
+// Conversation represents a discussion/thread within a forum.
 type Conversation struct {
-	Id          int64     `json:"id"`
+	ID          int64     `json:"id"`
 	Name        string    `json:"name"`
-	ForumId     int64     `json:"forumId, omitempty"`
+	ForumID     int64     `json:"forumId, omitempty"`
 	Author      int64     `json:"author,omitempty"`
 	DateCreated time.Time `json:"dateCreated,omitempty"`
 	ViewCount   int64     `json:"viewCount,omitempty"`
@@ -71,46 +122,69 @@ type Conversation struct {
 	Deleted     bool      `json:"isDeleted,omitempty"`
 }
 
+// Comment represents a post/comment that is attached to a conversation or other
+// type of content within a forum or on the wider site (a user may have a
+// comment as part of their profile to provide a bio). Comments are optionally
+// threaded by indicating which comment they are in reply to. Comments may also
+// have different versions, the latest version is presumed to be the live
+// version.
 type Comment struct {
-	Id          int64            `json:"id"`
-	OnType      string           `json:"onType,omitempty"`
-	OnId        int64            `json:"onId,omitempty"`
+	ID int64 `json:"id"`
+	Association
 	InReplyTo   int64            `json:"inReplyTo,omitempty"`
 	Author      int64            `json:"author,omitempty"`
 	DateCreated time.Time        `json:"dateCreated,omitempty"`
-	IpAddress   string           `json:"ipAddress,omitempty"`
+	IPAddress   string           `json:"ipAddress,omitempty"`
 	Moderated   bool             `json:"isModerated,omitempty"`
 	Deleted     bool             `json:"isDeleted,omitempty"`
 	Versions    []CommentVersion `json:"versions"`
 }
 
+// CommentVersion encapsulates the body of a comment. A single comment may have
+// many historical versions, and the latest version is presumed to be the live
+// one. Text is the raw body of the comment, which may be in plain text, bbcode,
+// Markdown or HTML or any combination thereof. It is assumed that any Text
+// value only contains common markup and not custom markup, by which we mean a
+// system exporting a comment is responsible for ensuring the exported value
+// is stripped of custom bbcodes, or the applicable conversion to HTML is done
+// by the export system.
 type CommentVersion struct {
 	Editor       int64     `json:"editor"`
 	DateModified time.Time `json:"dateModified,omitempty"`
 	Headline     string    `json:"headline,omitempty"`
 	Text         string    `json:"text"`
 	EditReason   string    `json:"editReason,omitempty"`
-	IpAddress    string    `json:"ipAddress,omitempty"`
+	IPAddress    string    `json:"ipAddress,omitempty"`
 }
 
+// Message describes a private message between one or more people
 type Message struct {
-	Id    int64  `json:"id"`
+	ID    int64  `json:"id"`
 	Name  string `json:"name"`
-	Users []Id   `json:"users,omitempty"`
+	Users []ID   `json:"users,omitempty"`
 }
 
+// Attachment represents a file that may be attached to a comment or other type.
 type Attachment struct {
-	Id          int64     `json:"id"`
-	OnType      string    `json:"onType,omitempty"`
-	OnId        int64     `json:"onId,omitempty"`
+	ID int64 `json:"id"`
+	Association
 	Author      int64     `json:"author,omitempty"`
 	DateCreated time.Time `json:"dateCreated,omitempty"`
 	Name        int64     `json:"name,omitempty"`
 	ContentSize uint64    `json:"contentSize,omitempty"`
-	ContentUrl  string    `json:"contentUrl"`
+	ContentURL  string    `json:"contentUrl"`
 }
 
+// Follow represents a like/follow/subscribe relationship between a user and
+// any content on the site
 type Follow struct {
 	Author  int64         `json:"author"`
 	Follows []Association `json:"follows"`
+}
+
+// Association describes any content by type and ID.
+// E.g. Association{OnType: "conversation", OnID: 123} means conversation 123
+type Association struct {
+	OnType string `json:"onType,omitempty"`
+	OnID   int64  `json:"onId,omitempty"`
 }
